25506d3017439a5cb5b90bac7b8c80f021f616fe
Ivnaa <48916663+Ivnaa@users.noreply.github.com>
2019-03-26 19:41:08 -0300

    Add files via upload

---
+++ Ex3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+    int main(){
+
+        printf("|- - -|- - -|- - -|- - -|- - -|- - -|- - -|- - -|\n");
+        printf("|B  TP|P  CP|B  BP|P  QP|B  KP|P  BP|B  CP|P  TP|\n");
+        printf("|- - -|- - -|- - -|- - -|- - -|- - -|- - -|- - -|\n");
+        printf("|P  PP|B  PP|P  PP|B  PP|P  PP|B  PP|P  PP|B  PP|\n");
+        printf("|- - -|- - -|- - -|- - -|- - -|- - -|- - -|- - -|\n");
+        printf("|B    |P    |B    |P    |B    |P    |B    |P    |\n");
+        printf("|- - -|- - -|- - -|- - -|- - -|- - -|- - -|- - -|\n");
+        printf("|P    |B    |P    |B    |P    |B    |P    |B    |\n");
+        printf("|- - -|- - -|- - -|- - -|- - -|- - -|- - -|- - -|\n");
+        printf("|B    |P    |B    |P    |B    |P    |B    |P    |\n");
+        printf("|- - -|- - -|- - -|- - -|- - -|- - -|- - -|- - -|\n");
+        printf("|P    |B    |P    |B    |P    |B    |P    |B    |\n");
+        printf("|- - -|- - -|- - -|- - -|- - -|- - -|- - -|- - -|\n");
+        printf("|B  PB|P  PB|B  PB|P  PB|B  PB|P  PB|B  PB|P  PB|\n");
+        printf("|- - -|- - -|- - -|- - -|- - -|- - -|- - -|- - -|\n");
+        printf("|P  TB|B  CB|P  BB|B  QB|P  KB|B  BB|P  CB|B  TB|\n");
+        printf("|- - -|- - -|- - -|- - -|- - -|- - -|- - -|- - -|\n");
+
+
+
+
+
+
+    return 0;
+    }
