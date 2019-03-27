d02e0df427e8f33722bb81b8bf4b6325c05cfc5c
JLMFrancelino <48970460+JLMFrancelino@users.noreply.github.com>
2019-03-26 21:36:27 -0300

    Add files via upload

---
+++ posição_peças_xadrez.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+int main() {
+
+printf("|----|----|----|----|----|----|----|----|\n");
+printf("|B TP|P CP|B BP|P DP|B RP|P BP|B CP|P TP|\n");
+printf("|----|----|----|----|----|----|----|----|\n");
+printf("|P PP|B PP|P PP|B PP|P PP|B PP|P PP|B PP|\n");
+printf("|----|----|----|----|----|----|----|----|\n");
+printf("|B   |P   |B   |P   |B   |P   |B   |P   |\n");
+printf("|----|----|----|----|----|----|----|----|\n");
+printf("|P   |B   |P   |B   |P   |B   |P   |B   |\n");
+printf("|----|----|----|----|----|----|----|----|\n");
+printf("|B   |P   |B   |P   |B   |P   |B   |P   |\n");
+printf("|----|----|----|----|----|----|----|----|\n");
+printf("|P   |B   |P   |B   |P   |B   |P   |B   |\n");
+printf("|----|----|----|----|----|----|----|----|\n");
+printf("|B PB|P PB|B PB|P PB|B PB|P PB|B PB|P PB|\n");
+printf("|----|----|----|----|----|----|----|----|\n");
+printf("|P TB|B CB|P BB|B DB|P RB|B BB|P CB|B TB|\n");
+printf("|----|----|----|----|----|----|----|----|\n");
+
+//printf("|B pe|P pe|B pe|P pe|B pe|P pe|B pe|P pe|\n");
+//printf("|P pe|B pe|P pe|B pe|P pe|B pe|P pe|B pe|\n");
+//printf("|B TB|P CV|B BB|P DB|B RB|P BB|B CV|P TB|\n");
+
+printf("\n\n\n\n");
+
+/* FAZENDO AGORA COM MATRIZ:
+int i;
+char t[42],p='|',d='-';
+
+for(i=1;i<42;i++)
+{
+    if(i%5==1)
+    {
+        t[i]=p;
+        printf("%c",t[i]);
+    //}
+    else
+    {
+        t[i]=d;
+        printf("%c",t[i]);
+    }
+}*/
+
+
+
+
+
+return 0;
+}
